4dedaad960d1ab640d005485f889071bc766119c
darefusion <dare@fusionintel.io>
2023-05-18 20:23:50 +0100

    update output

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "associate_public_ip_address" {
-    value = [aws_instance.oxla-ec2-instance.public_ip, aws_instance.oxla-bastion-host.public_ip]
+    value = [aws_instance.oxla-ec2-instance.public_ip, aws_instance.oxla-bastion-host.public_ip ]
   
 }
 
